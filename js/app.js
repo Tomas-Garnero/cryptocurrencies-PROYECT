5a43202b899d7b207ef0ea9e6ab165681913a626
unknown <tomygarnero9429@gmail.com>
2023-02-13 17:22:24 -0300

    Printing the rest of the Information

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const criptomonedasSelect = document.querySelector("#criptomonedas");
 const monedaSelect = document.querySelector("#moneda");
 const formulario = document.querySelector("#formulario");
+const resultado = document.querySelector("#resultado");
 
 const objBusqueda = {
     moneda: "",
@@ -88,5 +89,36 @@ function consultarAPI() {
 }
 
 function mostrarCotizacionHTML(cotizacion) {
-    console.log(cotizacion);
+
+    limpiarHTML();
+    
+    const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE } = cotizacion;
+
+    const precio = document.createElement("p");
+    precio.classList.add("precio");
+    precio.innerHTML = `El precio es: <span>${PRICE}</span>`;
+
+    const precioAlto = document.createElement("p");
+    precioAlto.innerHTML = `<p>Precio más alto del día: <span>${HIGHDAY}</span>`; 
+
+    const precioBajo = document.createElement("p");
+    precioBajo.innerHTML = `<p>Precio más bajo del día: <span>${LOWDAY}</span>`;
+    
+    const ultimasHoras = document.createElement("p");
+    ultimasHoras.innerHTML = `<p>Variación ultimas 24 horas: <span>${CHANGEPCT24HOUR}%</span>`;
+
+    const ultimaActualizacion = document.createElement("p");
+    ultimaActualizacion.innerHTML = `<p>Ultima actualización: <span>${LASTUPDATE}</span>`;
+
+    resultado.appendChild(precio);
+    resultado.appendChild(precioAlto);
+    resultado.appendChild(precioBajo);
+    resultado.appendChild(ultimasHoras);
+    resultado.appendChild(ultimaActualizacion);
+}
+
+function limpiarHTML() {
+    while(resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
 }
